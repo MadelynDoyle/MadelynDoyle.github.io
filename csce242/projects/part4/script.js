f6a5e0c4d2d80e328cd11fa0ecdcc1208b26b4a4
MadelynDoyle <doylemr@email.sc.edu>
2025-02-16 21:33:00 -0500

    project pt 3

---
+++ csce242/projects/part4/script.js
@@ -0,0 +1,8 @@
+function toggleNav() {
+    let nav = document.getElementById("nav-menu");
+    if (nav.style.display === "block") {
+        nav.style.display = "none"; 
+    } else {
+        nav.style.display = "block";
+    }
+}
